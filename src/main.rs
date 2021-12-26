d28b321bb64c684c06df58ec09898cfb5e790d1d
YJ Park <yjpark@gmail.com>
2021-12-26 20:49:42 +0800

    adding existing tabs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use notation_tab::prelude::*;
 
 pub mod test;
+pub mod songs;
+pub mod beginner;
 
 pub fn main() {
     write_tab(&test::new_tab(), "tabs/test.ron");
+    //songs
+    write_tab(&songs::scarborough_fair::new_tab(), "tabs/songs/scarborough_fair.ron");
+    //beginner
+    write_tab(&beginner::right_hand_1::new_tab(), "tabs/beginner/right_hand_1.ron");
 }
---
+++ src/songs/mod.rs
@@ -0,0 +1,1 @@
+pub mod scarborough_fair;
